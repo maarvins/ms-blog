546736be4acd5d6c80e5719b19b2e79a8777ecb3
Marcus Sena <maarvins.19@gmail.com>
2021-12-16 21:16:43 -0300

    chore: add Static Site Generator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import {GetServerSideProps} from "next"
+import {GetStaticProps} from "next"
 import Head from "next/head"
 import {SubscribeButton} from "../components/SubscribeButton"
 import girlPic from "../assets/mulher.svg"
@@ -37,7 +37,7 @@ export default function Home({product}: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1K7SbxDzteuG8UQ8v4B6FgJG")
 
   const product = {
@@ -51,6 +51,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 //24hours
   }
 }
